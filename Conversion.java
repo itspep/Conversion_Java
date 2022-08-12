c45ef79d56f2e718e209cfc6a5699cddb0d27db7
Mumuni Tahiru <peptahiru@gmail.com>
2022-08-12 12:17:59 +0000

    commit

--- Conversion.java
+++ Conversion.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class Conversion{
-    public static void main(String[]args){
+    public static void main(String[] args){
 int userChoice;
-double distance;
+double meters;
 Scanner input=new Scanner(System.in);
+
         System.out.println("This program converts meters in other units of conversion");
         System.out.println("Enter the unit of measurement you want to convert to:");
         System.out.println("1. Convert to Kilometers");
@@ -11,23 +12,36 @@ Scanner input=new Scanner(System.in);
         System.out.println("3. Convert to Feet");
         System.out.println("4. Quit the program");
         userChoice=input.nextInt();
-        System.out.println("Enter the distance you wish to convert");
-        distance=input.nextDouble();
         switch(userChoice)
         {
             case 1:
-                showKilometers(distance);
+                showKilometers();
                 break;
             case 2:
-                showInches(distance);
+                showInches();
                 break;
             case 3:
-                showFeet(distance);
+                showFeet();
             case 4:
                 System.exit(0);
         }
     }
-    public static void showKilometers(dis){
-
+    public static void showKilometers(){
+        System.out.print("Enter the distance in meters: ");
+        Scanner read =new Scanner(System.in);
+       double meters=read.nextDouble();
+        System.out.println(meters*0.001);
     }
+public static void showInches(){
+    System.out.print("Enter the distance in meters: ");
+    Scanner read =new Scanner(System.in);
+    double meters=read.nextDouble();
+    System.out.println(meters*39.37);
+        }
+public static void showFeet(){
+    System.out.print("Enter the distance in meters: ");
+    Scanner read =new Scanner(System.in);
+    double meters=read.nextDouble();
+    System.out.println(meters*3.281);
+        }
 }
